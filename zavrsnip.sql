21eca90c98cfac29fd5a5dfbcf93d4dfa38e18df
Stefan Lazarević <92578813+slazar810@users.noreply.github.com>
2021-11-30 14:35:00 +0100

    Ispravljanje atributa u tabelama i veza između istih
    Iz tabele korisnik uklonjen atribut narudzba, iz tabele kosarica uklonjen atribut korisnik, uklonjena veza između produkta i narudzbe, dignut vanjski ključ korisnik za povezivanje sa tabelom narudžba.

--- zavrsnip.sql
+++ zavrsnip.sql
@@ -21,8 +21,7 @@ create table korisnik(
     ime varchar(50) not null,
     prezime varchar(50) not null,
     oib char(11) not null,
-    email varchar(50) not null,
-    narudzba int not null
+    email varchar(50) not null
 );
 
 create table narudzba(
@@ -35,23 +34,19 @@ create table narudzba(
 create table kosarica(
     narudzba int not null,
     produkt int not null,
-    korisnik int not null,
-    ukupna_cijena decimal(18,2) not null
+    količina_cijena decimal(18,2) not null
 );
 
 
 alter table produkt add foreign key (kategorija) references kategorija(sifra);
 
-alter table korisnik add foreign key (narudzba) references narudzba(sifra);
 
-alter table narudzba add foreign key (produkt) references produkt(sifra);
+alter table narudzba add foreign key (korisnik) references korisnik(sifra);
 
 alter table kosarica add foreign key(narudzba) references narudzba(sifra);
 
-alter table kosarica add foreign key (korisnik) references korisnik(sifra);
 
 alter table kosarica add foreign key (produkt) references produkt (sifra);
-
 
 
 
@@ -66,9 +61,14 @@ insert into kategorija(sifra, naziv)values
 select * from produkt;
 
 insert into produkt(sifra, naziv, kategorija, opis, cijena)values
-(null, 'Rukavice Everlast', 1, '4oz crna', 299.99),
-(null, 'Whey protein - čokolada', 2, '908g crna' ,109.99);
+(null, 'Rukavice Everlast', 1, '4oz-crna', 299.99),
+(null, 'Whey protein - čokolada', 2, '908g-crna' ,109.99);
 
+select * from korisnik;
+
+insert into korisnik(sifra, ime, prezime, oib, email)values
+(null, 'Petar', 'Petrović', '88830680172', 'cistaproba@gmail.com'),
+(null, 'Darko', 'Darković', '82813639605', 'donniedarko@gmail.com');
 
 select * from narudzba;
 
@@ -76,14 +76,8 @@ insert into narudzba(sifra, produkt,korisnik, datum_narduzbe)values
 (null, 1, 1,'2021-11-20'),
 (null, 2, 2,'2021-11-21');
 
-select * from korisnik;
-
-insert into korisnik(sifra, ime, prezime, oib, email, narudzba)values
-(null, 'Petar', 'Petrović', '88830680172', 'cistaproba@gmail.com', 1),
-(null, 'Darko', 'Darković', '82813639605', 'donniedarko@gmail.com', 2);
-
 select * from kosarica;
 
-insert into kosarica(narudzba, korisnik, produkt, ukupna_cijena)values
-(1,1,1,299.99),
-(2,2,2,109.99);
+insert into kosarica(narudzba, produkt, količina_cijena)values
+(1,1,299.99),
+(2,2,109.99);
